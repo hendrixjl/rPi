45251fee80fa6783db60e70bf29f24b386d78ac5
hendrixjl <hendrix.john64@gmail.com>
2013-02-19 12:16:24 -0600

    Update bsp/bar.h
    declare BARs correctly. Put more code into .c

--- bsp/bar.h
+++ bsp/bar.h
@@ -9,48 +9,14 @@
 #define BAR_H_
 
 
-const int* IRQ_BAR;
-const int* ARM_TIMER_BAR;
-const int* GPIO_BAR;
-const int* BSC0_BAR;
-const int* BSC1_BAR;
-const int* BSC2_BAR;
-const int* PWM_BAR;
+volatile unsigned* IRQ_BAR;
+volatile unsigned* ARM_TIMER_BAR;
+volatile unsigned* GPIO_BAR;
+volatile unsigned* BSC0_BAR;
+volatile unsigned* BSC1_BAR;
+volatile unsigned* BSC2_BAR;
+volatile unsigned* PWM_BAR;
 
-    enum bar_t {
-        eIRQ_BAR =       0x2000B200,
-        eARM_TIMER_BAR = 0x2000B400,
-        eGPIO_BAR =      0x20200000
-        eBSC0_BAR = 	0x20205000,
-        eBSC1_BAR = 	0x20804000, // available?
-        eBSC2_BAR = 	0x20805000, // Not available on RPi
-        ePWM_BAR =	0x2020C000
-    };
-
-void straight_assign() // for bare metal
-{
-	IRQ_BAR = eIRQ_BAR;
-	ARM_TIMER_BAR = eARM_TIMER_BAR;
-	GPIO_BAR = eGPIO_BAR;
-	BSC0_BAR = eBSC0_BAR;
-	BSC1_BAR = eBSC1_BAR;
-	BSC2_BAR = 3BSC2_BAR;
-	PWM_BAR = ePWM_BAR;
-}
-
-void map_virtual_memory() // for linux
-{
-	
-}
-
-void init_bars()
-{
-
-#ifdef __BARE_METAL
-    striaght_assign();
-#else
-    map_virtual_memory();
-#endif
-}
+void init_bars();
 
 #endif /* BAR_H_ */
