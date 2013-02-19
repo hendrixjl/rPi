88c0593c949d156dcc64d186c021885b4af4247f
hendrixjl <hendrix.john64@gmail.com>
2013-02-19 11:43:20 -0600

    Update bsp/bar.h
    Add i2c mappings.

--- bsp/bar.h
+++ bsp/bar.h
@@ -13,6 +13,9 @@ enum bar_t {
 	IRQ_BAR =       0x2000B200,
 	ARM_TIMER_BAR = 0x2000B400,
 	GPIO_BAR =      0x20200000
+	BSC0_BAR = 	0x20205000,
+	BSC1_BAR = 	0x20804000,
+	BSC2_BAR = 	0x20805000 // Not available on RPi
 };
 
 #endif /* BAR_H_ */
