72757765011eecb544e175686bbe3d045ce6a2e6
hendrixjl <hendrix.john64@gmail.com>
2013-02-19 19:19:15 -0600

    init_bars is a C function

--- bsp/bar.h
+++ bsp/bar.h
@@ -8,7 +8,6 @@
 #ifndef BAR_H_
 #define BAR_H_
 
-
 volatile unsigned* IRQ_BAR;
 volatile unsigned* ARM_TIMER_BAR;
 volatile unsigned* GPIO_BAR;
@@ -17,6 +16,15 @@ volatile unsigned* BSC1_BAR;
 volatile unsigned* BSC2_BAR;
 volatile unsigned* PWM_BAR;
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 void init_bars();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* BAR_H_ */
