01f771afaa72c326c6970a5e4a41a17de84e2004
hendrixjl <hendrix.john64@gmail.com>
2013-02-19 20:30:53 -0600

    Remove debug prints

--- bsp/bar.c
+++ bsp/bar.c
@@ -83,24 +83,16 @@ volatile unsigned *mapRegisterMemory(int base)
 
 void map_virtual_memory() // for linux
 {
-	printf("%s:%d - \n", __func__, __LINE__);
 	volatile unsigned *b = mapRegisterMemory(eBASE);
 	IRQ_BAR = b + 200/4;
 //    IRQ_BAR = mapRegisterMemory(eIRQ_BAR);
-//	printf("%s:%d - \n", __func__, __LINE__);
 	ARM_TIMER_BAR = b + 400/4;
 //    ARM_TIMER_BAR = mapRegisterMemory(eARM_TIMER_BAR);
-	printf("%s:%d - \n", __func__, __LINE__);
     GPIO_BAR = mapRegisterMemory(eGPIO_BAR);
-	printf("%s:%d - \n", __func__, __LINE__);
     BSC0_BAR = mapRegisterMemory(eBSC0_BAR);
-	printf("%s:%d - \n", __func__, __LINE__);
     BSC1_BAR = mapRegisterMemory(eBSC1_BAR);
-	printf("%s:%d - \n", __func__, __LINE__);
     BSC2_BAR = mapRegisterMemory(eBSC2_BAR);
-	printf("%s:%d - \n", __func__, __LINE__);
     PWM_BAR = mapRegisterMemory(ePWM_BAR);
-	printf("%s:%d - \n", __func__, __LINE__);
 }
 #endif
 
