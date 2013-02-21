6451afc970c4ac610e35f3b0bf081215b627e09e
hendrixjl <hendrix.john64@gmail.com>
2013-02-21 10:10:27 -0600

    Update bsp/i2c.h
    Complete the conversion to a .cpp

--- bsp/i2c.h
+++ bsp/i2c.h
@@ -24,47 +24,27 @@ public:
 	 * @param bus
 	 * @return a i2c object address
 	 */
-	static i2c& setup(unsigned short bus) {
-		switch (bus)
-		{
-		case 0: return setup0();
-		case 1: return setup1();
-		case 2: return setup2();
-		default: return setup0();
-		}
-	}
+	static i2c& setup(unsigned short bus);
 
 	/**
 	 * Enable this i2c bus
 	 */
-	void enable()
-	{
-		bar_[CNTRL_REG] |= I2CEN;
-	}
+	void enable();
 
 	/**
 	 * Disable this i2c bus
 	 */
-	void disable()
-	{
-		bar_[CNTRL_REG] &= ~I2CEN;
-	}
+	void disable();
 
 	/**
 	 * Return the status bits of this i2c bus device
 	 */
-	unsigned int get_status() const
-	{
-		return bar_[STATUS_REG];
-	}
+	unsigned int get_status() const;
 
 	/**
 	 * Return the control bits of this i2c bus device
 	 */
-	unsigned int get_control() const
-	{
-		return bar_[CNTRL_REG];
-	}
+	unsigned int get_control() const;
 
 	/**
 	 * Send a command to a slave device
@@ -77,28 +57,7 @@ public:
 	unsigned command(unsigned char slaveaddr,
 			unsigned char cmd,
 			const unsigned char* data_addr=0,
-			unsigned data_size=0)
-	{
-		reset_txfer_done();
-		set_slave_address(slaveaddr);
-		set_data_len(1 + data_size);
-		initiate_write();
-		write_byte(cmd);
-		unsigned bytes_sent=1;
-		while (data_size>0)
-		{
-			if (write_byte(*data_addr))
-			{
-				data_addr++;
-				data_size--;
-				bytes_sent++;
-			}
-			// TODO check for error conditions
-		}
-		wait_for_done();
-		clear_fifo();
-		return bytes_sent;
-	}
+			unsigned data_size=0);
 
 	/**
 	 * Query a register on a slave device. Return
@@ -108,16 +67,7 @@ public:
 	 * @return reply
 	 */
 	unsigned char query(unsigned char slaveaddr,
-			unsigned char slave_register)
-	{
-		command(slaveaddr, slave_register);
-		initiate_read();
-		unsigned char b;
-		while (!read_byte(b)); // loop until success
-		clear_fifo();
-		// TODO error conditions?
-		return b;
-	}
+			unsigned char slave_register);
 
 
 	/**
@@ -132,289 +82,140 @@ public:
 	unsigned query(unsigned char slaveaddr,
 			unsigned char slave_register,
 			unsigned char* buffer,
-			unsigned buffer_size)
-	{
-		enum { READ_MULTIPLE=0x080 };
-		command(slaveaddr, slave_register | READ_MULTIPLE);
-		set_data_len(buffer_size);
-		initiate_read();
-		unsigned bytes_read=0;
-		while (buffer_size > 0)
-		{
-			if (read_byte(*buffer))
-			{
-				buffer_size--;
-				bytes_read++;
-				buffer++;
-			}
-			// TODO check for error conditions
-		}
-		clear_fifo();
-		return bytes_read;
-	}
+			unsigned buffer_size);
 
 	/**
 	 * Return the base address register for
 	 * the bus.
 	 */
-	unsigned int get_bar() const
-	{
-		return (unsigned int)bar_;
-	}
+	unsigned int get_bar() const;
 
 private:
-
-	/**
-	 * Register offsets from the BAR.
-	 */
-	enum registers_t {
-		CNTRL_REG = 0x0000,
-		STATUS_REG = 0x0001,
-		DLEN_REG = 0x0002,
-		SLAVE_ADDR_REG = 0x0003,
-		FIFO_REG = 0x0004,
-		CLOCK_DIV_REG = 0x0005,
-		DATA_DELAY_REG = 0x0006,
-		CLKT_REG = 0x0007
-	};
-
-	/**
-	 * For interpreting/setting the control bits
-	 * of the device
-	 */
-	enum control_bits_t {
-		I2CEN = 1 << 15,
-		INTR = 1 << 10,
-		INTT = 1 << 9,
-		INTD = 1 << 8,
-		ST = 1 << 7,
-		CLEAR = 1 << 4,
-		READ = 1
-	};
-
-	/**
-	 * For interpreting the status bits on the device
-	 */
-	enum status_bits_t {
-		CLKT=1<<9,
-		ERR=1<<8,
-		RXF=1<<7,
-		TXE=1<<6,
-		RXD=1<<5,
-		TXD=1<<4,
-		RXR=1<<3,
-		TXW=1<<2,
-		DONE=1<<1,
-		TA=1<<0
-	};
 
 
 	/**
 	 * Construct an i2c bus using
 	 * a base address register
 	 */
-	explicit i2c(volatile unsigned int *bar)
-	: bar_ (bar)
-	{
-		init();
-	}
+	explicit i2c(volatile unsigned int *bar);
 
 	/**
 	 * Initialize the bus, clear the slave
 	 * address, disable the bus.
 	 */
-	void init()
-	{
-		bar_[CNTRL_REG] = 0;
-		set_clk_divider();
-		set_delay();
-		set_clkt();
-		bar_[SLAVE_ADDR_REG] = 0;
-		bar_[DLEN_REG] = 0;
-	}
+	void init();
 
 	/**
 	 * Clear the FIFO on the bus device
 	 */
-	void clear_fifo() {
-		bar_[CNTRL_REG] |= CLEAR;
-	}
+	void clear_fifo();
 
 	/**
 	 * Clear the DONE bit for the bus device
 	 */
-	void clear_done() {
-		bar_[STATUS_REG] |= DONE;
-	}
+	void clear_done();
 
 	/**
 	 * Is there received data in the FIFO?
 	 * @return true if so
 	 */
-	bool rx_data_avail() const
-	{
-		return bar_[STATUS_REG] & RXD;
-	}
+	bool rx_data_avail() const;
 
 	/**
 	 * Is there room in the FIFO for transmit data?
 	 * @return true if so
 	 */
-	bool tx_room_avail() const
-	{
-		return bar_[STATUS_REG] & TXD;
-	}
+	bool tx_room_avail() const;
 
 	/**
 	 * Push a byte on the FIFO
 	 * @param byte
 	 */
-	void push_byte_on_fifo(unsigned char byte)
-	{
-		bar_[FIFO_REG] = byte;
-	}
+	void push_byte_on_fifo(unsigned char byte);
 
 	/**
 	 * Pop a byte from the FIFO
 	 */
-	unsigned char pop_byte_from_fifo() const
-	{
-		return bar_[FIFO_REG];
-	}
+	unsigned char pop_byte_from_fifo() const;
 
 	/**
 	 * Read a data byte from an i2c slave
 	 * @param byte
 	 * @return true if a byte was read
 	 */
-	bool read_byte(unsigned char& byte) const
-	{
-		if (rx_data_avail())
-		{
-			byte = pop_byte_from_fifo();
-			return true;
-		}
-		else
-		{
-			byte = 0x00;
-			return false;
-		}
-	}
+	bool read_byte(unsigned char& byte) const;
 
 	/**
 	 * Write a byte to an already-address i2c slave
 	 * @param byte
 	 * @return true if successful
 	 */
-	bool write_byte(unsigned char byte)
-	{
-		if (tx_room_avail())
-		{
-			push_byte_on_fifo(byte);
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+	bool write_byte(unsigned char byte);
 
 	/**
 	 * What is the state of an i2c transfer
 	 * @return true if the DONE bit is set
 	 */
-	bool txfer_done() const
-	{
-		return (bar_[STATUS_REG] & DONE);
-	}
+	bool txfer_done() const;
 
 	/**
 	 * Reset the transfer bit on the i2c bus device
 	 */
-	void reset_txfer_done() const
-	{
-		bar_[STATUS_REG] |= DONE;
-	}
+	void reset_txfer_done() const;
 
 	/**
 	 * Busy wait for the done bit to be set for the
 	 * i2c bus device
 	 */
-	void wait_for_done()
-	{
-		while (!txfer_done());
-		reset_txfer_done();
-	}
+	void wait_for_done();
 
 	/**
 	 * Set the slave address for the i2c bus device
 	 * @param slaveaddr
 	 */
-	void set_slave_address(unsigned char slaveaddr)
-	{
-		bar_[SLAVE_ADDR_REG] = slaveaddr;
-	}
+	void set_slave_address(unsigned char slaveaddr);
 
 	/**
 	 * Set the data length of an upcoming
 	 * i2c transfer
 	 * @param len
 	 */
-	void set_data_len(unsigned len)
-	{
-		bar_[DLEN_REG]=len;
-	}
+	void set_data_len(unsigned len);
 
 	/**
 	 * Initiate a write to an i2c slave device.
 	 */
-	void initiate_write()
-	{
-		bar_[CNTRL_REG] = (bar_[CNTRL_REG] & ~READ) | ST | CLEAR;
-	}
+	void initiate_write();
 
 	/**
 	 * Initiate a read from an i2c slave device
 	 */
-	void initiate_read()
-	{
-		bar_[CNTRL_REG] |= (ST | CLEAR | READ);
-	}
+	void initiate_read();
 
 	/**
 	 * Return the slave address currently
 	 * set for this i2c bus device
 	 * @return the slave address
 	 */
-	unsigned char get_slave_address() const {
-		return bar_[SLAVE_ADDR_REG];
-	}
+	unsigned char get_slave_address() const;
 
 	/**
 	 * Set the clock divider for the i2c bus device clock
 	 * @param div (defaults to 0x5DC [100 kHz])
 	 */
-	void set_clk_divider(unsigned short div=0x5DC) const {
-		bar_[CLOCK_DIV_REG] = div;
-	}
+	void set_clk_divider(unsigned short div=0x5DC) const;
 
 	/**
 	 * Set the delay for the i2c bus device.
 	 * @param delay (defaults to 0x0000 0030 0000 0030
 	 */
-	void set_delay(unsigned int delay=(0x030 << 16) + 0x030) const {
-		bar_[DATA_DELAY_REG] = delay;
-	}
+	void set_delay(unsigned int delay=(0x030 << 16) + 0x030) const;
 
 	/**
 	 * Set clkt for the i2c bus device.
 	 * @param clkt (defauts to 0x040)
 	 */
-	void set_clkt(unsigned char clkt=0x040) const {
-		bar_[CLKT] = clkt;
-	}
-
-	volatile unsigned int* bar_;
+	void set_clkt(unsigned char clkt=0x040) const;
 
 	i2c(const i2c&); // no copy
 	i2c& operator=(const i2c&); // no assign
@@ -422,27 +223,17 @@ private:
 	/**
 	 * Setup the i2c bus 0 and initialize it to disabled.
 	 */
-	static i2c& setup0() {
-		static i2c i2c0(BSC0_BAR);
-		i2c0.init();
-		return i2c0;
-	}
-	/**
+	static i2c& setup0();
+
+        /**
 	 * Setup the i2c bus 1 and initialize it to disabled.
 	 */
-	static i2c& setup1() {
-		static i2c i2c1(BSC1_BAR);
-		i2c1.init();
-		return i2c1;
-	}
-	/**
+	static i2c& setup1();
+
+        /**
 	 * Setup the i2c bus 2 and initialize it to disabled.
 	 */
-	static i2c& setup2() {
-		static i2c i2c2(BSC2_BAR);  // Not available on RPi
-		i2c2.init();  // Not available on RPi
-		return i2c2;
-	}
+	static i2c& setup2();
 
 };
 
