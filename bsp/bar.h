5b41f47d55187a5785210650e4845311948bcffc
hendrixjl <hendrix.john64@gmail.com>
2013-02-21 19:16:47 -0600

    bar variables must be extern

--- bsp/bar.h
+++ bsp/bar.h
@@ -8,13 +8,13 @@
 #ifndef BAR_H_
 #define BAR_H_
 
-volatile unsigned* IRQ_BAR;
-volatile unsigned* ARM_TIMER_BAR;
-volatile unsigned* GPIO_BAR;
-volatile unsigned* BSC0_BAR;
-volatile unsigned* BSC1_BAR;
-volatile unsigned* BSC2_BAR;
-volatile unsigned* PWM_BAR;
+extern volatile unsigned* IRQ_BAR;
+extern volatile unsigned* ARM_TIMER_BAR;
+extern volatile unsigned* GPIO_BAR;
+extern volatile unsigned* BSC0_BAR;
+extern volatile unsigned* BSC1_BAR;
+extern volatile unsigned* BSC2_BAR;
+extern volatile unsigned* PWM_BAR;
 
 #ifdef __cplusplus
 extern "C"
