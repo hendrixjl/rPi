ced152b316804cc8cd0bc930d0808558591cdfff
hendrixjl <hendrix.john64@gmail.com>
2013-02-22 09:40:15 -0600

    Create format.h
    Initial drop.

---
+++ format.h
@@ -0,0 +1,68 @@
+#ifndef FORMAT_H_
+#define FORMAT_H_
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/**
+ * Convert an unsigned integer to string.
+ * @param buffer - the data buffer to store the string in
+ * @param blen - the available space in the buffer
+ * @param n - the number to convert
+ * @return The number of characters used. -1 if error.
+ */
+int unsigned_to_string(char* buffer, size_t blen, unsigned n);
+
+/**
+ * Convert an integer to string.
+ * @param buffer - the data buffer to store the string in
+ * @param blen - the available space in the buffer
+ * @param n - the number to convert
+ * @return The number of characters used. -1 if error.
+ */
+int int_to_string(char* buffer, size_t blen, int n);
+
+/**
+ * Convert an unsigned integer to a hex string.
+ * @param buffer - the data buffer to store the string in
+ * @param blen - the available space in the buffer
+ * @param n - the number to convert
+ * @return The number of characters used. -1 if error.
+ */
+int unsigned_to_xstring(char* buffer, size_t blen, unsigned n);
+
+/**
+ * Convert decimal string to unsigned integer
+ * @param n - Pointer to the resulting unsigned
+ * @param buffer - The string holding the number
+ * @return The number of characters used. -1 if error.
+ */
+int string_to_unsigned(unsigned* n, const char* buffer);
+
+/**
+ * Convert a hex string to unsigned integer.
+ * @param n - Pointer to the resulting unsigned
+ * @param buffer - The string holding the number
+ * @return The number of characters used. -1 if error.
+ */
+int xstring_to_unsigned(unsigned* n, const char* buffer);
+
+
+/**
+ * Convert decimal string to integer
+ * @param n - Pointer to the resulting unsigned
+ * @param buffer - The string holding the number
+ * @return The number of characters used. -1 if error.
+ */
+int string_to_unsigned(int* n, const char* buffer);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* FORMAT_H_ */
