4fc8b777c620444079e7fa27c1cbc662300c9aab
hendrixjl <hendrix.john64@gmail.com>
2013-02-23 10:23:55 -0600

    try edge detection

--- main.cpp
+++ main.cpp
@@ -24,8 +24,9 @@ void pause_till_signal()
 {
 	gpio_fsel(PIN_23, INTPUT);
 	gpio_set_pud(PIN_23, ENABLE_PULL_UP_CNTRL);
-//	gpio_set_event_detect(PIN_23, FALLING_EDGE_DETECT);
-	while (gpio_get_level(PIN_23) == PIN_HIGH);
+	gpio_set_event_detect(PIN_23, FALLING_EDGE_DETECT);
+	gpio_clear_event_detected(PIN_23);
+	while (gpio_event_detected(PIN_23));
 	cout << "Signal!" << endl;
 }
 
