85f6a0b301d1e7d419c8424f322b32c31baf5076
hendrixjl <hendrix.john64@gmail.com>
2013-02-24 15:09:31 -0600

    add file back in, named correctly.

---
+++ utils/timeUtils.h
@@ -0,0 +1,23 @@
+/*
+ * timeutils.h
+ *
+ *  Created on: Feb 24, 2013
+ *      Author: jonathanhendrix
+ */
+
+#ifndef TIMEUTILS_H_
+#define TIMEUTILS_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void busyloop(unsigned int n);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TIMEUTILS_H_ */
