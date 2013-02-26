7d97e7469bfe6dfe7c2c94f0a0e08dbb3b23e3ef
hendrixjl <hendrix.john64@gmail.com>
2013-02-26 09:42:55 -0600

    Create mytypes.h
    intial

---
+++ utils/mytypes.h
@@ -0,0 +1,12 @@
+#ifndef __MYTYPES_H__
+#define __MYTYPES_H__
+
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+
+typedef int int32_t;
+typedef short int16_t;
+typedef char int8_t;
+
+#endif
