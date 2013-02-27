8c63799b72dbbe114032fa887b6803efcada72f9
hendrixjl <hendrix.john64@gmail.com>
2013-02-27 13:29:08 -0600

    Update utils/timeUtils.c
    define udelay

--- utils/timeUtils.c
+++ utils/timeUtils.c
@@ -6,6 +6,25 @@
  */
 #include "timeUtils.h"
 
+#ifdef __BARE_METAL__
+
+enum { USECS_TO_CYCLES=100 }; // just a guess
+
+void udelay(uint32_t useconds)
+{
+	busyloop(useconds * USECS_TO_CYCLES);
+}
+
+#else
+
+#include <unistd.h>
+
+void udelay(uint32_t useconds)
+{
+    usleep(useconds);	
+}
+
+#endif
 
 void busyloop(unsigned int n)
 {
