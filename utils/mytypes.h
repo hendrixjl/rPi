a834f962ab7be35e41aa326559cdaf8adfb1bcb8
hendrixjl <hendrix.john64@gmail.com>
2013-02-27 17:53:57 -0600

    Use stddef.h for size_t for bare metal and linux, unix

--- utils/mytypes.h
+++ utils/mytypes.h
@@ -1,6 +1,8 @@
 #ifndef __MYTYPES_H__
 #define __MYTYPES_H__
 
+#include <stddef.h>
+
 #ifdef __BARE_METAL__
 typedef unsigned int uint32_t;
 typedef unsigned short uint16_t;
@@ -10,12 +12,9 @@ typedef int int32_t;
 typedef short int16_t;
 typedef char int8_t;
 
-typedef unsigned int size_t;
-
 #else
 
 #include <stdint.h>
-#include <stdlib.h>
 
 #endif
 
