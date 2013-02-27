d5e6a5c4d7ba707eaec008bda4f95d604d507431
hendrixjl <hendrix.john64@gmail.com>
2013-02-27 10:36:03 -0600

    Update utils/mytypes.h
    Use stdint.h if not in bare metal

--- utils/mytypes.h
+++ utils/mytypes.h
@@ -1,6 +1,7 @@
 #ifndef __MYTYPES_H__
 #define __MYTYPES_H__
 
+#ifdef __BARE_METAL__
 typedef unsigned int uint32_t;
 typedef unsigned short uint16_t;
 typedef unsigned char uint8_t;
@@ -8,5 +9,11 @@ typedef unsigned char uint8_t;
 typedef int int32_t;
 typedef short int16_t;
 typedef char int8_t;
+
+#else
+
+#include <stdint.h>
+
+#endif
 
 #endif
