a2cd7c25ba91701505d6c1ec6a590939820c30df
hendrixjl <hendrix.john64@gmail.com>
2013-02-27 12:03:37 -0600

    Update linux/main.cpp
    Put pause in the maneuver program.

--- linux/main.cpp
+++ linux/main.cpp
@@ -1,25 +1,12 @@
 #include "bar.h"
-#include "gpio.h"
 #include "stdout.h"
 
-
-extern void pause_till_signal();
 
 extern void maneuver_program();
 
 int main()
 {
 	init_bars();
-	initHardware();
 
-	gpio_set_fsel(PIN_18, ALT1);
-
-	write("Waiting for signal");
-	writeln();
-	pause_till_signal();
-	write("Signal!");
-	writeln();
-	
 	maneuver_program();
-
 }
