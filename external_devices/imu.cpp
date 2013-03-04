dd8c4605ebe676ef93db96d4a0de261ad8737237
hendrixjl <hendrix.john64@gmail.com>
2013-03-04 09:45:19 -0600

    Update imu.cpp
    Set up basic logic for sensor loop.

--- external_devices/imu.cpp
+++ external_devices/imu.cpp
@@ -1,5 +1,5 @@
-
 #include "imu.h"
+#include "stdout.h"
 
 enum {
 	LSM303_A=0x19,
@@ -14,25 +14,35 @@ imu::imu(i2c& ani2c)
   inclination_(0.0),
   roll_(0.0)
 {
+	l3gd20 gyro(L3GD20,i2c1);
+	gyro.enable();
+
+	lsm303dlh accel(LSM303_M, LSM303_A, i2c1);
+	accel.enable();
 }
 
 imu::~imu()
 {
 }
 
-//		short temp; unsigned char status; short xa; short ya; short za;
-//		gyro.measurements(temp, status, xa, ya, za);
-//		int16_t accels[3] = {};
-//		accel.getAcc(accels);
-//		int16_t mags[3] = {};
-//		accel.getMag(mags);
-//		cout << "Gyro: " << temp << " " << xa << " " << ya << " " << za
-//				<< " accel: " << accels[0] << " " << accels[1] << " " << accels[2]
-//				<< " mags: " << mags[0] << " " << mags[1] << " " << mags[2]
-//				<< endl;
+void imu::integrate()
+{
+	write(getTime()); write(',');
+	int16_t angles[3];
+	gyro.measurements(angles);
+	write(angles[0]); write(',');
+	write(angles[1]); write(',');
+	write(angles[2]); write(',');
 
-//l3gd20 gyro(L3GD20,i2c1);
-//gyro.enable();
-//
-//lsm303dlh accel(LSM303_M, LSM303_A, i2c1);
-//accel.enable();
+	int16_t accels[3] = {};
+	accel.getAcc(accels);
+	write(accels[0]); write(',');
+	write(accels[1]); write(',');
+	write(accels[2]); write(',');
+
+	int16_t mags[3] = {};
+	accel.getMag(mags);
+	write(mags[0]); write(',');
+	write(mags[1]); write(',');
+	writeln(mags[2]); write(',');
+}
