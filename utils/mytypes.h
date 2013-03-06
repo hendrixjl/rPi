3db502e2c45ca14de49a55e7c20ab2d023e1a69d
hendrixjl <hendrix.john64@gmail.com>
2013-03-05 20:19:02 -0600

    add uint64_t

--- utils/mytypes.h
+++ utils/mytypes.h
@@ -4,6 +4,8 @@
 #include <stddef.h>
 
 #ifdef __BARE_METAL__
+
+typedef unsigned long long uint64_t;
 typedef unsigned int uint32_t;
 typedef unsigned short uint16_t;
 typedef unsigned char uint8_t;
