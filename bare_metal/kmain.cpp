116d7e0e1f1d457e5a6ebca801ca1bf4177d7522
hendrixjl <hendrix.john64@gmail.com>
2013-03-06 18:34:50 -0600

    slightly improve time scheme

--- bare_metal/kmain.cpp
+++ bare_metal/kmain.cpp
@@ -6,33 +6,27 @@ extern void maneuver_program();
 extern "C" int kmain();
 
 enum {
-	MICROSECONDS_IN_SECOND = 1000000U
+	CYCLES_IN_TIME_TICK = 1000U,
 };
 
-unsigned seconds;
+unsigned time_ticks;
 
 void c_irq_handler()
 {
-	++seconds;
+	++time_ticks;
 }
 
-extern "C" unsigned long long getTime();
-
-unsigned long long getTime()
-{
-	return seconds*MICROSECONDS_IN_SECOND + arm_timer_val();
-}
 
 int kmain()
 {
-	seconds = 0;
+	time_ticks = 0;
     init_bars();
 
     enum {
     	ARM_TIMER_DIV = 0x000000F9U
     };
-    arm_timer_setup(MICROSECONDS_IN_SECOND-1,
-    		MICROSECONDS_IN_SECOND-1,
+    arm_timer_setup(CYCLES_IN_TIME_TICK-1,
+    		CYCLES_IN_TIME_TICK-1,
     		ARM_TIMER_DIV,
     		PRESCALE_IS_CLOCK_DIV_1,
     		TWENTY_THREE_BITS);
--- bare_metal/timeUtils.c
+++ bare_metal/timeUtils.c
@@ -1,9 +1,27 @@
 #include "timeUtils.h"
+#include "arm_timer.h"
 
+enum {
+	TIME_TICKS_IN_USEC=1
+};
 
-enum { USECS_TO_CYCLES=1 }; // just a guess
+unsigned arm_timer_to_usec() {
+	return arm_timer_val()/TIME_TICKS_IN_USEC;
+}
 
 void udelay(uint32_t useconds)
 {
-	busyloop(useconds * USECS_TO_CYCLES);
+	enum { USECS_IN_CLCK_CYCLES };
+	busyloop(useconds * USECS_IN_CLCK_CYCLES);
+}
+
+extern unsigned time_ticks;
+
+unsigned long long getTime()
+{
+	enum {
+		MICROSECONDS_IN_SECOND = 1000000U
+	};
+
+	return time_ticks * TIME_TICKS_IN_USEC;
 }
