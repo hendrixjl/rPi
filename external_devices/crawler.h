4fc76e22686b74dbbae9dd10321720e19805d21f
hendrixjl <hendrix.john64@gmail.com>
2013-10-14 19:27:52 -0500

    Update crawler.h
    enum class. Add pam.

--- external_devices/crawler.h
+++ external_devices/crawler.h
@@ -1,35 +1,35 @@
-
 #ifndef CRAWLER_H_
 #define CRAWLER_H_
 
 #include "mcp23008.h"
+#include "pca9685.h"
 #include "mytypes.h"
 
 class i2c;
 
 class crawler {
 public:
-	enum turn_t {
-		STOP_TURN,
-		RIGHT_TURN,
-		LEFT_TURN,
-		NO_CHANGE_TURN
+	enum class turn {
+		STOP,
+		RIGHT,
+		LEFT,
+		NO_CHANGE
 	};
 
-	enum direction_t {
-		STOP_DIRECTION,
-		FORWARD_DIRECTION,
-		BACKWARD_DIRECTION,
-		NO_CHANGE_DIRECTION
+	enum class direction {
+		STOP,
+		FORWARD,
+		BACKWARD,
+		NO_CHANGE
 	};
 
 	typedef size_t power_t; // 0 .. 100
 
 	struct maneuver_t
 	{
-	    direction_t direction;
+	    direction direction;
 	    power_t direction_power;
-	    turn_t turn;
+	    turn turn;
 	    power_t turn_power;
 	};
 
@@ -41,11 +41,12 @@ public:
 
 private:
 	mcp23008 gpio_;
-	direction_t current_direction_;
-	turn_t current_turn_;
+	pca9685 pwm_;
+	direction current_direction_;
+	turn current_turn_;
 
-	uint8_t turn_to_olat(turn_t turn);
-	uint8_t direction_to_olat(direction_t dir);
+	uint8_t turn_to_olat(turn turn);
+	uint8_t direction_to_olat(direction dir);
 
 	crawler(const crawler&)=delete; // no copy
 	crawler& operator=(const crawler&)=delete; // no assign
