3d1f064428b079247edced356b2f75ed079960bb
hendrixjl <hendrix.john64@gmail.com>
2013-10-16 16:49:39 -0500

    Create vehicle_controller.h
    Initial

---
+++ external_devices/vehicle_controller.h
@@ -0,0 +1,35 @@
+#ifndef __VEHICLE_CONTROLLER_H__
+#define __VEHICLE_CONTROLLER_H__
+
+class vehicle_controller
+{
+public:
+    enum class fb_dir { FORWARD, STOP, BACKWARD };
+    enum class fl_dir { RIGHT, STOP, LEFT };
+    
+    vehicle_controller();
+
+    void fb_control(fb_dir, power) {
+        switch fb_dir {
+        case FOWARD: fb_ctr_->go(); fb_power_->set_duty(power); break;
+        case STOP: fb_ctr_->stop(); fb_power_->set_duty(0); break;
+        case FOWARD: fb_ctr_->back(); fb_power_->set_duty(power); break;
+        }
+    }
+    
+    void rl_control(rl_dir, power) {
+        switch rl_dir {
+        case RIGHT: rl_ctr_->go(); fb_power_->set_duty(power); break;
+        case STOP: rl_ctr_->stop(); fb_power_->set_duty(0); break;
+        case LEFT: rl_ctr_->back(); fb_power_->set_duty(power); break;
+    }
+
+private:
+    unique_ptr<direction_ctrl> fb_ctr_;
+    unique_ptr<pwm_driver> fb_power_;
+    unique_ptr<direction_ctrl> rl_ctr_;
+    unique_ptr<pwm_driver> rl_power_;
+};
+
+
+#endif
