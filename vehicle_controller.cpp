ad66969e60e687437720386c3a9ba0990bea39c3
hendrixjl <hendrix.john64@gmail.com>
2013-10-28 11:33:33 -0500

    Update vehicle_controller.cpp
    Use constructor initializer list.

--- vehicle_controller.cpp
+++ vehicle_controller.cpp
@@ -6,23 +6,19 @@
 
 using namespace std;
 
+shared_ptr<pca9685> get_pca9685_shared()
+{
+    enum { PCA9685_ADDRESS = 0x40 };
+    static auto pca9685shared = 
+        make_shared<pca9685>(PCA9685_ADDRESS, i2c::setup1());
+    return pca9685shared;
+}
+
 vehicle_controller::vehicle_controller()
 : fb_ctr_(make_unique<gpio_dir_controller>()),
-  fb_power_(),
+  fb_power_(make_unique<pca_9685_pwm>(pca9685::pwmled::LED0, pca9685shared)),
   rl_ctr_(make_unique<gpio_dir_controller>()),
-  rl_power_()
+  rl_power_(make_unique<pca_9685_pwm>(pca9685::pwmled::LED1, pca9685shared))
 {
-	enum { PCA9685_ADDRESS = 0x40 };
-	auto pca9685shared = 
-	    make_shared<pca9685>(PCA9685_ADDRESS, i2c::setup1());
-
-	fb_power_ = 
-	    make_unique<pca_9685_pwm>(pca9685::pwmled::LED0, 
-	             pca9685shared);
-	             
-	rl_power_ = 
-	    make_unique<pca_9685_pwm>(pca9685::pwmled::LED1, 
-	             pca9685shared);
-	             
 }
 
